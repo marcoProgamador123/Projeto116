5938211458f82a7b234960c3aa7e34c5f56fffab
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-12-17 21:03:46 -0300

    concluido

--- family tree/app.py
+++ family tree/app.py
@@ -9,21 +9,46 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    nome = "" # escreva seu nome
-    idade = "" # escreva sua idade
+    nome = "Marco Antonio" # escreva seu nome
+    idade = "13" # escreva sua idade
 
     return render_template('index.html' , nome = nome , idade = idade)
 
 # defina a rota para a página do pai
-
+@app.route("/pai")
+def pai():
+    
+    nome = "Antonio Marcos"
+    idade = 47
+    
+    return render_template('pai.html' , nome = nome , idade = idade)      
 
 # defina a rota para a página da mãe
-
-
-# defina a rota para a página do amigo
-
+@app.route("/mae")
+def mae():
+    
+    nome = "Danielle"
+    idade = 41
+    
+    return render_template('mae.html' , nome = nome , idade = idade)      
 
 # adicione outras rotas, se você quiser
+@app.route("/irmao")
+def irmao1():
+    
+    nome = "Marco Andre"
+    idade = 2
+    
+    return render_template('irmao.html' , nome = nome , idade = idade)      
+
+@app.route("/irmao2")
+def irmao2():
+    
+    nome = "Marco Aurelio"
+    idade = 10
+    
+    return render_template('irmao2.html' , nome = nome , idade = idade)      
+
 
 
 
